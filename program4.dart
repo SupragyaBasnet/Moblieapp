8829d1189785fb4370d41cfaad3f6d232a59eeb2
SupragyaBasnet <125958893+SupragyaBasnet@users.noreply.github.com>
2024-11-17 12:58:20 +0545

    fourth commit

---
+++ program4.dart
@@ -0,0 +1,13 @@
+void main() {
+  // Step 1: Declare the number
+  int num = 5; // You can change this to any number
+
+  // Step 2: Calculate the square
+  int square = num * num;
+
+  // Step 3: Print the result with explanation
+  print('You entered: $num');
+  print('Calculating the square...');
+  print('$num multiplied by $num equals $square');
+  print('The square of $num is $square.');
+}

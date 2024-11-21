d7f6de42caa5fb2b72a4cc52b293a6d77c3332a2
SupragyaBasnet <125958893+SupragyaBasnet@users.noreply.github.com>
2024-11-21 08:46:53 +0545

    second si

---
+++ tempCodeRunnerFile.dart
@@ -0,0 +1,11 @@
+void main() {
+//   double principal = 1000; // Example principal amount
+//   double time = 2; // Example time in years
+//   double rate = 5; // Example rate of interest
+
+//   print("Simple interest is ${si(principal, time, rate)}");
+// }
+
+// double si(double p, double t, double r) {
+//   return (p * t * r) / 100;
+// }
